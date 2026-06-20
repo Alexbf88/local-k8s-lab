f20fa50904a041af95a49612627a868699c07f17
Borges <alexandrebf4388@gmail.com>
2026-06-20 11:20:55 -0300

    ci: add automated multi-stage docker build job to integration workflow

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -94,13 +94,17 @@ DB_HOST = os.environ.get("DB_HOST", "127.0.0.1")  # Fallbacks to localhost loop 
 DB_PORT = os.environ.get("DB_PORT", "5432")
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": DB_NAME,
-        "USER": DB_USER,
-        "PASSWORD": DB_PASSWORD,
-        "HOST": DB_HOST,
-        "PORT": DB_PORT,
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": DB_NAME,
+#        "USER": DB_USER,
+#        "PASSWORD": DB_PASSWORD,
+#        "HOST": DB_HOST,
+#        "PORT": DB_PORT,
+#    }
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
