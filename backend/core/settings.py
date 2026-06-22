3814fe27fbf6287339c4cd1c6d92e7f4366a7dca
Borges <alexandrebf4388@gmail.com>
2026-06-22 16:57:59 -0300

    chore: update postgres cluster configuration to 1Gi with longhorn storage

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,11 +11,21 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+# set casting and default values
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -94,18 +104,7 @@ DB_HOST = os.environ.get("DB_HOST", "127.0.0.1")  # Fallbacks to localhost loop 
 DB_PORT = os.environ.get("DB_PORT", "5432")
 
 DATABASES = {
-    #    "default": {
-    #        "ENGINE": "django.db.backends.postgresql",
-    #        "NAME": DB_NAME,
-    #        "USER": DB_USER,
-    #        "PASSWORD": DB_PASSWORD,
-    #        "HOST": DB_HOST,
-    #        "PORT": DB_PORT,
-    #    }
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': env.db('DATABASE_URL')
 }
 
 
